b05c0f795dab3a7dc14832bdbc1fccbc0ba15cbe
Téo Calvo <teocalvo2@gmail.com>
2025-01-26 19:59:36 -0300

    feat: machine learning

---
+++ cursos/machine_learning.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+def curso_machine_learning():
+    about = """
+    Curso completo e gratuito de Machine Learning para pessoas iniciantes na área da Ciência de Dados.
+
+    As aulas acontecerão nos dias 05/05 e 16/05, ai vivo na [Twitch](https://twitch.tv/teomewhy), onde, posteriormente será disponibilizado no [YouTube](https://youtube.com/@teomewhy).
+
+    O uso de técnicas voltadas à Aprendizado de Máquina tem sido cada dia mais comuns em diferentes mercados e indústricas. Dominar os principais conceitos te ajudará a alcançar não só objetivos da sua empresa, mas também, de crescimento profissional.
+    Neste curso, você aprenderá os principais conceitos de Machine Learning, mais do que apenas apertar os botões corretos. Falaremos sobre realmente como a máquina aprender e como podemos avaliar esse aprendizados a partir de diferentes métricas.
+
+    Você entenderá de uma vez por todas como cada técnica funciona, e as métricas de performance. Quando falarem sobre curva ROC, você terá condições de explicar como funciona e porque normalmente é uma métrica melhor que a acurácia. 
+
+    [Adicione na sua agenda](https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=MTl0bnVjbGJldHNsNjU4dWg0ajgxc3BhdXVfMjAyNTA1MDVUMTEwMDAwWiB0ZW9AdGVvbWV3aHkub3Jn&tmsrc=teo%40teomewhy.org&scp=ALL) para participar conosco ao vivo.
+    """
+    st.markdown(about)
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import streamlit as st
-from git_github import git_github
-from programacao import curso_pandas, curso_python
-from estatistica import curso_estatistica_pt01, curso_estatistica_pt02
+from cursos.git_github import git_github
+from cursos.programacao import curso_pandas, curso_python
+from cursos.estatistica import curso_estatistica_pt01, curso_estatistica_pt02
+from cursos.machine_learning import curso_machine_learning
 
 st.markdown("# Téo Me Why - 2025")
 
@@ -22,3 +23,6 @@ with st.expander("Estatística: Parte 01 - 31/03 a 04/04"):
 
 with st.expander("Estatística: Parte 02 - 14/04 a 18/04"):
     curso_estatistica_pt02()
+
+with st.expander("Machine Learning - 05/05 a 16/05"):
+    curso_machine_learning()
