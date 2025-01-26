ef8e5fe7c28a2227b566884b238816f7a4b3e1b4
Téo Calvo <teocalvo2@gmail.com>
2025-01-26 19:49:31 -0300

    feat: Estatística pt 02

--- estatistica.py
+++ estatistica.py
@@ -5,20 +5,32 @@ def curso_estatistica_pt01():
     about = """
     Curso gratuido de Estatística (Parte I) para pessoas iniciantes na área de dados.
 
-    essas aulas acontecerão ao vivo entre os dias 31/03 e 04/04, na [Twitch](https://twitch.tv/teomewhy), onde, posteriormente será disponibilizado no [YouTube](https://youtube.com/@teomewhy).
+    Essas aulas acontecerão ao vivo entre os dias 31/03 e 04/04, na [Twitch](https://twitch.tv/teomewhy), onde, posteriormente será disponibilizado no [YouTube](https://youtube.com/@teomewhy).
 
     Estatística é uma das principais disciplinas para a área de dados. Desde análise simples, mas tambem, toda base para avançarmos em aprendizado de máquina, se faz necessário o conhecimento de métodos e modelos estatísticos.
     
-    Vamos fornecer o conhecimento sobre Estatística de forma introdutória. Mas não subestime nosso material.
-    
-    Cobriremos:
-        - Conceitos de população (parâmetro) e amostra (estimadores);
-        - Estatística descritiva;
-        - Representações gráficas;
-        - Probabilidade;
-        - Intervalo de confiânça;
-        - Testes de hipótese;
+    Vamos fornecer o conhecimento sobre Estatística de forma introdutória. Mas não subestime nosso material. Cobriremos:
+
+    - Conceitos de população (parâmetro) e amostra (estimadores);
+    - Estatística descritiva;
+    - Representações gráficas;
+    - Probabilidade;
+    - Intervalo de confiânça;
+    - Testes de hipótese;
 
     [Adicione na sua agenda](https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=NTZpa3FxbzRja2ZjOWVjdmdzNXYycjh1c3BfMjAyNTAzMzFUMTEwMDAwWiB0ZW9AdGVvbWV3aHkub3Jn&tmsrc=teo%40teomewhy.org&scp=ALL) para participar conosco ao vivo.
+    """
+    st.markdown(about)
+
+def curso_estatistica_pt02():
+
+    about = """
+    Curso gratuido de Estatística (Parte I) para pessoas iniciantes na área de dados.
+
+    Essas aulas acontecerão ao vivo entre os dias 14/04 e 18/04, na [Twitch](https://twitch.tv/teomewhy), onde, posteriormente será disponibilizado no [YouTube](https://youtube.com/@teomewhy).
+
+    Dessa vez, vamos utilizar e aplicar toda a teoria apresentada anteriormente, em vias computacionais. Vai ser uma chance interessante para sair da teoria, tirando as coisas do papel, e colocar para rodar no computador.
+
+    [Adicione na sua agenda](https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=MDVpcHZoYWUybW1uZ2d2NzliZW50ZWxsdDFfMjAyNTA0MTRUMTEwMDAwWiB0ZW9AdGVvbWV3aHkub3Jn&tmsrc=teo%40teomewhy.org&scp=ALL) para participar conosco ao vivo.
     """
     st.markdown(about)
--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import streamlit as st
 from git_github import git_github
 from programacao import curso_pandas, curso_python
-from estatistica import curso_estatistica_pt01
+from estatistica import curso_estatistica_pt01, curso_estatistica_pt02
 
 st.markdown("# Téo Me Why - 2025")
 
 st.markdown("## Cursos")
 st.markdown("Acompanhe aqui nossos cursos realizados conforme avançamos e tudo o que nos espera durante este ano.")
 
-with st.expander("Git e GitHub"):
+with st.expander("Git e GitHub - Em andamento"):
     git_github()
 
-with st.expander("Python - em breve"):
+with st.expander("Python - 10/02 a 14/02"):
     curso_python()
 
-with st.expander("Pandas - em breve"):
+with st.expander("Pandas - 10/03 a 14/03"):
     curso_pandas()
 
-with st.expander("Estatística - em breve"):
+with st.expander("Estatística: Parte 01 - 31/03 a 04/04"):
     curso_estatistica_pt01()
+
+with st.expander("Estatística: Parte 02 - 14/04 a 18/04"):
+    curso_estatistica_pt02()
