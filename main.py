62172f739aa9cff700dcfad861e28fc25f5521aa
teo <teocalvo2@gmail.com>
2025-02-04 20:12:43 -0300

    feat: page title

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from databases import models
 db = models.SessionLocal()
 models.create_tables()
 
+st.set_page_config(page_title="Téo Me Why - 2025")
 st.title("Téo Me Why - 2025")
 st.markdown("Boas vindas ao nosso calendário de cursos! Por aqui você poderá tanto acompanhar nossa agenda, mas tambem conferir todos conteúdos que geramos em 2025.")
 
