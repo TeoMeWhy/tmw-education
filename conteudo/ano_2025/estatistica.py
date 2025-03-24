554b370810396b6b8b6c3c8d2aec9fabf0539c2a
Teo Calvo <teocalvo2@gmail.com>
2025-03-24 09:31:29 -0300

    Update estatistica.py

--- conteudo/ano_2025/estatistica.py
+++ conteudo/ano_2025/estatistica.py
@@ -3,12 +3,12 @@ import streamlit as st
 
 def curso_estatistica(course_eps:pd.DataFrame):
 
-    with st.expander("Estatística Básica: 05/04 a 16/04"):
+    with st.expander("Estatística Básica: 05/05 a 16/05"):
 
         about = """
         Curso gratuido de Estatística (Parte I) para pessoas iniciantes na área de dados.
 
-        Essas aulas acontecerão ao vivo entre os dias 05/04 e 16/04, na [Twitch](https://twitch.tv/teomewhy), onde, posteriormente será disponibilizado no [YouTube](https://youtube.com/@teomewhy).
+        Essas aulas acontecerão ao vivo entre os dias 05/05 e 16/05, na [Twitch](https://twitch.tv/teomewhy), onde, posteriormente será disponibilizado no [YouTube](https://youtube.com/@teomewhy).
 
         Estatística é uma das principais disciplinas para a área de dados. Desde análise simples, mas tambem, toda base para avançarmos em aprendizado de máquina, se faz necessário o conhecimento de métodos e modelos estatísticos.
         
