c31a4cfd0c348e8f80c07c3bf2b1819730e63094
Teo Calvo <teocalvo2@gmail.com>
2025-07-09 11:29:30 -0300

    feat: itens para compra e venda

--- points/points.py
+++ points/points.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import uuid
 
 POINTS_URI = os.getenv("POINTS_URI")
 
@@ -20,6 +21,66 @@ def create_user_points(**kwargs):
     
     if resp.status_code == 201:
         return resp.json()["customer"]
+    
+    else:
+        return resp.json()
+
+
+def make_rpg_store_transaction(tmw_id, items, buyer=True):
+    
+    data = {
+        "transaction_id":str(uuid.uuid4()),
+        "customer_id": tmw_id,
+        "points": sum(item["price"] for item in items),
+        "system_origin":"cursos",
+    }
+
+    data['points'] = -1*data['points'] if buyer else data['points']
+
+    data_items = []
+
+    for i in items:
+        data_items.append({
+            "id": str(uuid.uuid4()),
+            "IdTransaction": data["transaction_id"],
+            "CodProduct": f"{i['id']:0>3} - {i['name']}",
+            "QtdeProduct": 1,
+            "VlProduct": i["price"]
+        })
+
+    data["products"] = data_items
+    return data
+
+
+def make_rpg_store_transaction_refound(tmw_id, items):
+    data = {
+        "transaction_id":str(uuid.uuid4()),
+        "customer_id": tmw_id,
+        "points": sum(item["price"] for item in items),
+        "system_origin":"cursos",
+    }
+
+    data_items = []
+
+    for i in items:
+        data_items.append({
+            "id": str(uuid.uuid4()),
+            "IdTransaction": data["transaction_id"],
+            "CodProduct": f"{i['id']:0>3} - {i['name']} - refound",
+            "QtdeProduct": 1,
+            "VlProduct": i["price"]
+        })
+
+    data["products"] = data_items
+    return data
+
+
+def post_transaction(**kwargs):
+    url = f"{POINTS_URI}/transactions"
+    resp = requests.post(url, json=kwargs)
+    
+    if resp.status_code == 201:
+        return resp.json()
     
     else:
         return resp.json()
