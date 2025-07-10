32086654e1b4309136f0b5fcdea14a42f2e1de98
Teo Calvo <teocalvo2@gmail.com>
2025-07-10 18:02:11 -0300

    feat: reward para cursos finalizados

--- pages/perfil.py
+++ pages/perfil.py
@@ -16,7 +16,9 @@ from login import twitch_login
 from databases import models
 
 
-def show_points_infos(db:orm.Session, tmw_id:str)->bool:
+def show_points_infos(db:orm.Session)->bool:
+
+    tmw_id = st.session_state.get("tmw_id", None)
 
     if not tmw_id:
         return False
@@ -54,7 +56,7 @@ def show_points_infos(db:orm.Session, tmw_id:str)->bool:
     return True
     
 
-def integrate_or_create_tmw(user):
+def integrate_or_create_tmw(db: orm.Session, user):
     payload = {user.platformName:user.platformUserID}
     
     data = points.get_user_points(**payload)
@@ -69,7 +71,17 @@ def integrate_or_create_tmw(user):
     time.sleep(1)
     st.rerun()
 
-def show_rpg(tmw_id:str, twitch_name:str):
+
+def show_rpg():
+
+    tmw_id = st.session_state.get("tmw_id", None)
+    twitch_name = st.session_state.get("twitch_data", {}).get("display_name", None)
+
+    st.markdown("#### Minigame RPG")
+
+    if not tmw_id or not twitch_name:
+        st.warning("Você precisa vincular seu perfil ao ecossistema para acessar o RPG.")
+        return
 
     with st.expander("Personagem RPG", expanded=False):
         char = heroes.get_creature(tmw_id)
@@ -111,12 +123,56 @@ def show_uncompleted_courses(courses_eps, user_courses_progress):
                                        alt_text=alt_text)
 
 
-def show_completed_courses(courses_eps, user_courses_progress):
+def show_rewards(courses:pd.DataFrame):
+    
+    st.markdown("#### Recompensas")
+    user_rewards = models.get_user_rewards(db, user_id=st.session_state['user'].userID)
+    user_rewards = [i.rewardID for i in user_rewards]
+
+    tmw_id = st.session_state.get("tmw_user", {}).get("uuid", None)
+    user_id = st.session_state['user'].userID
+    if not tmw_id:
+        st.warning("Você precisa vincular seu perfil ao ecossistema para resgatar recompensas.")
+        return
+
+    courses_slugs = courses[~courses['courseSlug'].isin(user_rewards)]["courseSlug"].tolist()
+    courses_slugs = {f"{i.replace('-', ' ').title()}": i for i in courses_slugs}
+
+    if len(courses_slugs) == 0:
+        st.markdown("Você não tem recompensas disponíveis. Finalize mais cursos para resgatar novas recompensas!")
+        return
+
+    check_reward = st.pills("Você tem recompensas te esperando:", courses_slugs.keys(), selection_mode="multi")
+
+    if len(check_reward) > 0:
+        if st.button("Confirme seu resgate!"):
+
+            reward_ids = [courses_slugs[i] for i in check_reward]
+
+            data = points.make_reward_transaction(tmw_id=tmw_id, reward_ids=reward_ids)
+            resp = points.post_transaction(**data)
+            if "error" in resp:
+                st.error(f"Erro ao resgatar recompensas: {resp['error']}")
+                return
+
+            new_rewards = [models.UserRewards(userID=user_id, rewardID=i) for i in reward_ids]
+            db.add_all(new_rewards)
+            db.commit()
+            
+            st.success(f"Recomensa(s) resgatada(s) com sucesso! Foram adicionados {data['points']} cubos!")
+            time.sleep(2)
+            st.rerun()
+
+
+def show_completed_courses(courses_eps:pd.DataFrame, user_courses_progress):
     courses = courses_eps[courses_eps['pctCompleted'] == 1]
-    if courses.shape[0]:
+    if not courses.empty:
+        
         st.markdown("""## Cursos finalizados""")
         for s in courses['courseSlug']:
             utils.load_and_show_course(db, s, user_courses_progress)
+        show_rewards(courses)
+
 
 
 def make_user_progress_completed(db: orm.Session, user_courses_progress:pd.DataFrame):
@@ -136,13 +192,14 @@ def make_user_progress_completed(db: orm.Session, user_courses_progress:pd.DataF
                                                          how='inner',
                                                          left_on='courseSlug',
                                                          right_on='slug')
-                                                 .drop("slug", axis=1))
+                                                   .drop("slug", axis=1))
 
     user_courses_complete['pctCompleted'] = user_courses_complete['qtdeEpsUser'] / user_courses_complete['qtdEpsCourse']
     return user_courses_complete
 
 
-def show_user_progress_courses(db:orm.Session, user_id):
+def show_user_progress_courses(db:orm.Session):
+    user_id = st.session_state['user'].userID
     user_courses_progress = utils.get_courses_dataframe(db=db, user_id=user_id)
     user_courses_complete = make_user_progress_completed(db=db, user_courses_progress=user_courses_progress)
 
@@ -160,18 +217,21 @@ if 'user' not in st.session_state:
     st.stop()
 
 user = st.session_state["user"]
+
 tmw_id = models.get_tmw_id(db, user.userID)
+st.session_state["tmw_id"] = tmw_id
 
 twitch_data = twitch_login.get_twitch_infos(st.session_state['token'])
 if "data" in twitch_data and len(twitch_data["data"]) > 0:
     name = twitch_data["data"][0]["display_name"]
+    st.session_state["twitch_data"] = twitch_data["data"][0]
 else:
     st.error("Não foi possível obter o nome do usuário da Twitch.")
 
-if not show_points_infos(db, tmw_id):
-    b = st.button("Clique aqui para vincular seu perfil ao ecossistema", on_click=lambda: integrate_or_create_tmw(user))
+if not show_points_infos(db):
+    if st.button("Clique aqui para vincular seu perfil ao ecossistema"):
+        integrate_or_create_tmw(db, user)
 
-show_rpg(tmw_id=tmw_id, twitch_name=name)
-
-show_user_progress_courses(db=db, user_id=user.userID)
+show_rpg()
+show_user_progress_courses(db=db)
 
