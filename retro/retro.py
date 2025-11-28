61d4b54ac8533f8ac04e02787966de7bd41bad31
Téo Calvo <teocalvo2@gmail.com>
2025-11-28 11:34:04 -0300

    fix: perfil de retrospectiva

--- retro/retro.py
+++ retro/retro.py
@@ -5,7 +5,8 @@ import os
 def get_retro(uuid, name):
     uri = os.getenv("RETRO_URI")
     resp = requests.get(f"{uri}/retro?name={uuid}&name={name}&source=streamlit")
-    if resp.status_code == 200:
+    if resp.status_code == 202:
         return resp.json()
     else:
+        print(resp.text)
         return False
