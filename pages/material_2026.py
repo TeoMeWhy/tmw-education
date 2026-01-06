b33393f05d27250b2d6444b3b58f2ccb2dcb555c
Téo Calvo <teocalvo2@gmail.com>
2026-01-06 09:42:52 -0300

    feat: curso de go

--- pages/material_2026.py
+++ pages/material_2026.py
@@ -14,6 +14,7 @@ def show_cursos_2026(db:orm.Session):
 
     |Conteúdo| Descrição | Tipo | Data |
     |---|---|---|---|
+    |[Introdução a Go](https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=NzExazg2MXRxdnR2MnVvaDgyZmFzazdoMThfMjAyNjAxMTlUMTIwMDAwWiB0ZW9AdGVvbWV3aHkub3Jn&tmsrc=teo%40teomewhy.org&scp=ALL)|Curso introdutório da sintaxe e particularidades da linguagem Go, com uma visão de uma pessoa de Dados.| Curso | 19/01/2026 9AM |
     |[Plataforma Machine Learning](https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=NW5tYWM5cWY2MzA4OWNkc3VlZjRodjRuMHFfMjAyNjAyMDJUMTIwMDAwWiB0ZW9AdGVvbWV3aHkub3Jn&tmsrc=teo%40teomewhy.org&scp=ALL)|Construção de uma plataforma de ML para simplificar deploy e monitoramento de modelos| Projeto | 02/02/2026 9AM |
     |[Fórmula 1](https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=M29hZ25pNG10bmIzYm5vNjBjYmM2dXZyazRfMjAyNjAyMjNUMTIwMDAwWiB0ZW9AdGVvbWV3aHkub3Jn&tmsrc=teo%40teomewhy.org&scp=ALL)|Criação de Lakehouse na Nekt para dados da Fórmula 1| Projeto | 23/02/2026 9AM |
     |[Dota2](https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=NnVpNGVjYWN1OTY4ZXEycTA0aW0ybWtkNjFfMjAyNjAzMzBUMTIwMDAwWiB0ZW9AdGVvbWV3aHkub3Jn&tmsrc=teo%40teomewhy.org&scp=ALL)|Coleta, armazenamento e processamento de dados para criação de Dashboards e Modelos de ML| Projeto | 30/03/2026 9AM |
